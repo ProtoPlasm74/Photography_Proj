d943b3742d200e8c2c5c1fe8381e0f43189b0698
ProtoPlasm74 <zacmorgan@Mac-Pro.local>
2016-11-05 10:23:38 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
