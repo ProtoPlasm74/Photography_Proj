3b8b39d52d5f9f23a15bd62e8b95a85247b85f54
Zac Morgan <zac@debian.morgancentral.net>
2016-11-05 16:32:42 -0400

    commit from debian

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+ 
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,10 +9,6 @@ def Home():
 @app.route('/about')
 def about():
     return render_template("about.html")
-
-@app.route('/resume')
-def resume():
-    return render_template("resume.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
